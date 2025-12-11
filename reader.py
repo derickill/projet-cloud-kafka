4f0d6cab30c516cba0678b85e2b1acf3dc59dbbb
derickill <derick.attoubou@epita.fr>
2025-12-11 09:08:46 +0100

    Update reader.py

--- reader.py
+++ reader.py
@@ -27,6 +27,44 @@ def lire_stats():
     ca = conn.execute("SELECT magasin, SUM(total) FROM tickets GROUP BY magasin").fetchall()
     conn.close()
     return {"CA": ca}
+    
+@app.get("/stats/nombre_articles_magasin")
+def nombre_articles_magasin():
+    conn = get_db_connection()
+    data = conn.execute("""
+        SELECT t.magasin, COUNT(a.id_article) AS nombre_articles
+        FROM tickets t
+        JOIN article a ON t.id_ticket = a.id_ticket
+        GROUP BY t.magasin
+    """).fetchall()
+    conn.close()
+    return {"nombre_articles_par_magasin": data}
+
+@app.get("/stats/prix_par_article_magasin")
+def prix_par_article_magasin():
+    conn = get_db_connection()
+    data = conn.execute("""
+        SELECT t.magasin, a.Article, SUM(a.quantite * a.prix_unitaire) AS total_article
+        FROM tickets t
+        JOIN article a ON t.id_ticket = a.id_ticket
+        GROUP BY t.magasin, a.Article
+    """).fetchall()
+    conn.close()
+    return {"prix_par_article_par_magasin": data}
+
+@app.get("/stats/quantite_articles_magasin")
+def quantite_articles_magasin():
+    conn = get_db_connection()
+    data = conn.execute("""
+        SELECT t.magasin, SUM(a.quantite) AS quantite_totale
+        FROM tickets t
+        JOIN article a ON t.id_ticket = a.id_ticket
+        GROUP BY t.magasin
+    """).fetchAll()
+    conn.close()
+    return {"quantite_articles_par_magasin": data}
+
+
 
 if __name__ == "__main__":
     # Port 8001 obligatoire car le producer utilise le 8000)
